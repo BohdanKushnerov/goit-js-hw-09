d85c119d9635da2256953c42add2840303cf206e
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-02-01 14:45:39 +0200

    Add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+
+
+const colorSwitcher = {
+  intervalId: null,
+  isActive: false,
+
+  start() {
+    if(this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+        bodyRef.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+      console.log(this.intervalId)
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  }
+}
+
+startBtn.addEventListener('click', ()=> {
+  colorSwitcher.start()
+});
+stopBtn.addEventListener('click', ()=> {
+  colorSwitcher.stop()
+})
+
+// console.log(colorSwitcher.intervalId)
+
+
+
 
