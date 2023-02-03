3a324667060c80962c7bc87edbd7864025c11848
Bohdan Kushnerov <bohdan.kushnerov@gmail.com>
2023-02-03 16:30:49 +0200

    Add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,20 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const myInput = document.getElementById("datetime-picker");
-const btnRef = document.querySelector('button[data-start]')
+const myInput = document.getElementById('datetime-picker');
+const btnRef = document.querySelector('button[data-start]');
 
 const daysRef = document.querySelector('span[data-days]');
 const hoursRef = document.querySelector('span[data-hours]');
 const minutesRef = document.querySelector('span[data-minutes]');
 const secondsRef = document.querySelector('span[data-seconds]');
-// console.log(secondsRef);
+
+btnRef.addEventListener('click', () => {
+  timer.start();
+});
+
+let userDate = 0;
 btnRef.disabled = true;
 
 const options = {
@@ -17,60 +23,105 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const chooseTime = selectedDates[0].getTime();
-    const currentTime = options.defaultDate.getTime();
-    const selectedTime = chooseTime - currentTime;
-
-    if(chooseTime <= currentTime) {
-      window.alert("Please choose a date in the future");
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      Notify.failure('Please choose a date in the future');
     } else {
-      // console.log(selectedTime);
       btnRef.disabled = false;
-      // convertMs(selectedTime);
-
-      const { days, hours, minutes, seconds } = convertMs(selectedTime);
-
-      daysRef.textContent = `${days}`;
-      hoursRef.textContent = `${hours}`;
-      minutesRef.textContent = `${minutes}`;
-      secondsRef.textContent = `${seconds}`;
-
-      console.log(daysRef);
+      userDate = selectedDates[0].getTime();
     }
   },
 };
 
-const fp = flatpickr(myInput, options);  // flatpickr
+const fp = flatpickr(myInput, options); // flatpickr
 
+class Timer  {
+  constructor({onTextChange}) {
+    this.intervalId = null,
+    this.isActive = false,
+    this.onTextChange = onTextChange
+  }
 
+  start() {
+    btnRef.disabled = true;
+    if(this.isActive) {
+      return
+    }
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+    this.isActive = true,
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    this.intervalId = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const selectedTime = userDate - currentTime;
+    
+    const time = this.convertMs(selectedTime);
+    this.onTextChange(time);
 
-  return { days, hours, minutes, seconds };
+    // const { days, hours, minutes, seconds } = this.convertMs(selectedTime);
+    console.log(time)
+
+    // daysRef.textContent = `${days}`;
+    // hoursRef.textContent = `${hours}`;
+    // minutesRef.textContent = `${minutes}`;
+    // secondsRef.textContent = `${seconds}`;
+
+      if (selectedTime <= 1000) {
+        this.stop();
+      }
+    }, 1000);
+  };
+
+  stop() {
+    btnRef.disabled = false;
+    this.isActive = false;
+
+    const time = this.convertMs(0);
+    this.onTextChange(time);
+
+    clearInterval(this.intervalId);
+    Notify.success('Timer is finised!');
+    return;
+  };
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+  
+    return { days, hours, minutes, seconds };
+  };
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, 0);
+  }
+};
+
+const timer = new Timer({
+  onTextChange: makeTextContent
+});
+
+function makeTextContent({ days, hours, minutes, seconds }) {
+// const { days, hours, minutes, seconds } = timer.convertMs(time);
+
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minutesRef.textContent = `${minutes}`;
+  secondsRef.textContent = `${seconds}`;
 }
 
 
-// Відлік часу
-// Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
 
-// Кількість днів може складатися з більше, ніж двох цифр.
-// Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто 00:00:00:00.
-// НЕ БУДЕМО УСКЛАДНЮВАТИ
-// Якщо таймер запущений, для того щоб вибрати нову дату і перезапустити його - необхідно перезавантажити сторінку.
-
-// Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
 
